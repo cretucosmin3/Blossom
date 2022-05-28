66ad8a697118672f4c0d9ac357fdf958262329b2
Cosmin Cretu <cosmin.cretu@minstersoft.co.uk>
2022-05-28 22:00:34 +0100

    Jumping button

--- src/Testing/Views/PrettyUi.cs
+++ src/Testing/Views/PrettyUi.cs
@@ -19,6 +19,7 @@ namespace Rux.Testing
 
         VisualElement Text;
         VisualElement Button;
+        VisualElement Ground;
 
         public PrettyUi() : base("PrettyUi View") { }
 
@@ -30,8 +31,8 @@ namespace Rux.Testing
             Text = new VisualElement()
             {
                 Name = "LoginText",
-                Text = "Welcome back!",
-                Transform = new(550 - 200, 240, 400, 80)
+                Text = "Click to jump",
+                Transform = new(550 - 200, 180, 400, 80)
                 {
                     Anchor = Anchor.Top,
                     FixedWidth = false,
@@ -52,9 +53,10 @@ namespace Rux.Testing
             Button = new VisualElement()
             {
                 Name = "Button",
-                Text = "Submit",
+                Text = "Hello",
                 Transform = new(550 - 100, 300, 200, 40)
                 {
+                    Anchor = Anchor.Bottom,
                     FixedWidth = true,
                     FixedHeight = true,
                     ValidateOnAnchor = false,
@@ -77,43 +79,83 @@ namespace Rux.Testing
                 },
             };
 
+            Ground = new VisualElement()
+            {
+                Name = "Ground",
+                Transform = new(400, 462f, 300, 60)
+                {
+                    Anchor = Anchor.Bottom,
+                    FixedWidth = true,
+                    FixedHeight = true,
+                    ValidateOnAnchor = false,
+                },
+                Style = new()
+                {
+                    BackColor = new SKColor(30, 30, 40),
+                    Text = new()
+                    {
+                        Size = 28f,
+                        Alignment = TextAlign.Center,
+                        Color = SKColors.White,
+                    },
+                    Border = new()
+                    {
+                        Roundness = 2f,
+                        Width = 0.5f,
+                        Color = SKColors.Black,
+                    }
+                },
+            };
+
             Button.Events.OnMouseClick += (btn, pos) =>
             {
-                Console.WriteLine("Button Clicked");
-                Text.Text = $"+{counter += 1}";
+                time = 700f; // ms
+                jumpDistance += 150f;
+                sw.Restart();
             };
 
             Button.Events.OnMouseDown += (btn, pos) =>
             {
                 Button.Style.Border.Width = 1f;
                 Button.Style.BackColor = new SKColor(96 + 10, 16 + 10, 16 + 10);
-                Text.Style.Text.Size += 6f;
             };
 
             Button.Events.OnMouseUp += (btn, pos) =>
             {
                 Button.Style.Border.Width = 0.6f;
                 Button.Style.BackColor = new SKColor(96, 16, 16);
-                Text.Style.Text.Size -= 6f;
             };
 
             AddElement(Text);
             AddElement(Button);
+            AddElement(Ground);
 
             Loop += Update;
         }
 
-        float time = 1000f; // ms
-        float jumpDistance = 100f;
+        float time = 700f; // ms
+        float jumpDistance = 150f;
         Stopwatch sw = Stopwatch.StartNew();
         private void Update()
         {
             var p = MathF.PI * (sw.ElapsedMilliseconds / time);
             var v = MathF.Sin(p);
-            Button.Transform.Y = 420f - jumpDistance * MathF.Sin(v);
+            Button.Transform.Y = 420f - jumpDistance * v;
 
-            if(sw.ElapsedMilliseconds > time)
+            if (sw.ElapsedMilliseconds > time && time > 60)
+            {
                 sw.Restart();
+                time /= 1.6f;
+                jumpDistance /= 2f;
+                Ground.Text = $"{counter += 1}";
+                return;
+            }
+            if(time < 60)
+            {
+                time = 700f;
+                jumpDistance = 0;
+                sw.Stop();
+            }
         }
 
         float smoothLerp(float from, float to, float progress)
