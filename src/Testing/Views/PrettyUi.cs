dbe3c3cf04aadd8725fdc854fc4eae549f9a836b
Cosmin Cretu <cosmin.cretu@minstersoft.co.uk>
2022-08-11 13:32:31 +0100

    Styles idea

--- src/Testing/Views/PrettyUi.cs
+++ src/Testing/Views/PrettyUi.cs
@@ -94,7 +94,38 @@ namespace Rux.Testing
                 Button.Style.Border.Width = 1;
             };
 
-            RenderChanges(() => {
+            List<VisualStyle> styles = new(){
+                VisualStyle.OnEvent("LoginButton", VisualEvents.MouseHover, (VisualElement elm) => {
+                    elm.Style.BackColor = new SKColor(50, 50, 50, 255);
+                    elm.Style.Text.Color = SKColors.White;
+                    elm.Style.Border.Color = SKColors.White;
+                }),
+                VisualStyle.Base("LoginButton", (VisualElement elm) => {
+                    elm.Style.BackColor = SKColors.WhiteSmoke;
+                    elm.Style.Text.Color = SKColors.Black;
+                    elm.Style.Border.Color = SKColors.Black;
+                }),
+                VisualStyle.Base("LoginForm", new ElementStyle()
+                {
+                    BackColor = SKColors.WhiteSmoke,
+                    Border = new()
+                    {
+                        Roundness = 10,
+                        Width = 1,
+                        Color = SKColors.Black,
+                    },
+                    Text = new()
+                    {
+                        Size = 25f,
+                        Weight = 400,
+                        Alignment = TextAlign.Center,
+                        Color = SKColors.Black,
+                    }
+                }),
+            };
+
+            RenderChanges(() =>
+            {
                 Text.Text = "Hello world!";
                 Button.Text = "Click me";
             });
@@ -112,5 +143,57 @@ namespace Rux.Testing
         {
             return a + f * (b - a);
         }
+    }
+}
+
+
+public enum VisualEvents
+{
+    MouseDown,
+    MouseUp,
+    MouseMove,
+    MouseEnter,
+    MouseLeave,
+    MouseHover,
+}
+
+public class VisualStyle
+{
+    private string _Class;
+    private VisualEvents _Event;
+    private Action<VisualElement> _StyleEvent;
+    private ElementStyle _Style;
+
+    public VisualStyle(string className)
+    {
+        _Class = className;
+    }
+
+    public VisualStyle(string className, VisualEvents eventName)
+    {
+        _Class = className;
+        _Event = eventName;
+    }
+
+    public static VisualStyle Base(string className, ElementStyle style)
+    {
+        return new VisualStyle(className) {
+            _Style = style,
+        };
+    }
+
+    public static VisualStyle Base(string className, Action<VisualElement> styleEvent)
+    {
+        return new VisualStyle(className) {
+            _StyleEvent = styleEvent
+        };
+    }
+
+    public static VisualStyle OnEvent(string className, VisualEvents eventName, Action<VisualElement> styleEvent)
+    {
+        return new VisualStyle(className) {
+            _Event = eventName,
+            _StyleEvent = styleEvent
+        };
     }
 }
